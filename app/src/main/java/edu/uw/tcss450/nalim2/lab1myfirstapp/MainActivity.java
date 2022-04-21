c13aca5310e0c611153a8caa9542b4dce3bec3b6
Foziya99 <fozi@uw.edu>
2022-04-21 03:14:50 -0700

    3:14

--- app/src/main/java/edu/uw/tcss450/nalim2/lab1myfirstapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/nalim2/lab1myfirstapp/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     // Comment Added by Artur
     // Comment Added by Michael
+
+
+    //comment Added by Foziya
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
